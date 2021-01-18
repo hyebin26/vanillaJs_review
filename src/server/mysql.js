61b7c49a36dd23bb2a96702a214a5d2568a62b06
(hyebin) <(sksp4334@naver.com)>
2021-01-18 18:15:49 +0900

    add environment variable

--- src/server/mysql.js
+++ src/server/mysql.js
@@ -1,11 +1,13 @@
+const path = require("path");
+require("dotenv").config({ path: __dirname + "/../../.env" });
 const mysql = require("mysql");
 
 const conn = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "@@da8611zi",
-  database: "review",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
 });
 
 module.exports = conn;
