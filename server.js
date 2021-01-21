c3d58485b41a55116691a82ff1c8093cdf0663e5
(hyebin) <(sksp4334@naver.com)>
2021-01-21 16:23:00 +0900

    add heroku7

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const conn = require("./src/server/mysql");
+const router = require("./src/router/main")(app);
+const PORT = process.env.DB_PORT;
+
+app.use(express.static(__dirname + "../../../public"));
+app.use(express.static(__dirname + "../../"));
+
+// app.get("/review", function (req, res) {
+//   res.sendFile(path.join(router));
+//   //__dirname : It will resolve to your project folder.
+// });
+
+app.set("views", "../../views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+const server = app.listen(3306 || 3000, function () {
+  console.log("Express server has started on port 3000");
+});
+
+console.log(process.env.PROT);
--- src/server/server.js
+++ src/server/server.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const app = express();
-const conn = require("./mysql");
-const router = require("../router/main")(app);
-const PORT = process.env.DB_PORT;
+// const express = require("express");
+// const app = express();
+// const conn = require("./mysql");
+// const router = require("../router/main")(app);
+// const PORT = process.env.DB_PORT;
 
-app.use(express.static(__dirname + "../../../public"));
-app.use(express.static(__dirname + "../../"));
+// app.use(express.static(__dirname + "../../../public"));
+// app.use(express.static(__dirname + "../../"));
 
-// app.get("/review", function (req, res) {
-//   res.sendFile(path.join(router));
-//   //__dirname : It will resolve to your project folder.
+// // app.get("/review", function (req, res) {
+// //   res.sendFile(path.join(router));
+// //   //__dirname : It will resolve to your project folder.
+// // });
+
+// app.set("views", "../../views");
+// app.set("view engine", "ejs");
+// app.engine("html", require("ejs").renderFile);
+
+// const server = app.listen(3306 || 3000, function () {
+//   console.log("Express server has started on port 3000");
 // });
 
-app.set("views", "../../views");
-app.set("view engine", "ejs");
-app.engine("html", require("ejs").renderFile);
-
-const server = app.listen(3306 || 3000, function () {
-  console.log("Express server has started on port 3000");
-});
-
-console.log(process.env.PROT);
+// console.log(process.env.PROT);
