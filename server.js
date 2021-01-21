66dae24be6a73044fab8457792ad6b2c0d9d5b01
(hyebin) <(sksp4334@naver.com)>
2021-01-21 16:35:01 +0900

    heroku8

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const conn = require("./src/server/mysql");
-const router = require("./src/router/main")(app);
-const PORT = process.env.DB_PORT;
+// const router = require("./src/router/main")(app);
 
-app.use(express.static(__dirname + "../../../public"));
-app.use(express.static(__dirname + "../../"));
+// app.use(express.static(__dirname + "../../../public"));
+// app.use(express.static(__dirname + "../../"));
 
 // app.get("/review", function (req, res) {
 //   res.sendFile(path.join(router));
@@ -16,8 +15,9 @@ app.set("views", "../../views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
+app.get("/", (req, res) => {
+  res.send("SUces");
+});
 const server = app.listen(3306 || 3000, function () {
   console.log("Express server has started on port 3000");
 });
-
-console.log(process.env.PROT);
