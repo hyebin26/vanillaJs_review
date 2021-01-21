5b0e699b917b8c6e4a31326c93358b8f4d992e5e
(hyebin) <(sksp4334@naver.com)>
2021-01-21 19:55:34 +0900

    add heroku12

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const conn = require("./mysql.js");
-const router = require("./src/router/main")(app);
+//const router = require("./src/router/main")(app);
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/src"));
 
-app.set("views", "./views");
-app.set("view engine", "ejs");
-app.engine("html", require("ejs").renderFile);
+// app.set("views", "./views");
+// app.set("view engine", "ejs");
+// app.engine("html", require("ejs").renderFile);
 
-app.get("/123", (req, res) => {
+app.get("/", (req, res) => {
   conn.query("select * from reviewData", (err, result) => {
     console.log(result);
     res.send(result);
