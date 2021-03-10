59bc3fdd5c0041295632a34b7a64b269374af892
hyebin-Hwang <67263146+hyebin-Hwang@users.noreply.github.com>
2021-03-10 15:00:25 +0900

    update server.js
    delete //

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
 const conn = require("./mysql.js");
-//const router = require("./src/router/main")(app);
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/src"));
 
-// app.set("views", "./views");
-// app.set("view engine", "ejs");
-// app.engine("html", require("ejs").renderFile);
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
