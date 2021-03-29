e730beece5e721d816fbc0b0ec786e11b848e61e
(hyebin) <(sksp4334@naver.com)>
2021-03-29 20:54:18 +0900

    revise edit_review fetch url and editReviewForm.html js.css file route

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### 이번에 한 것
 
-1. update html, js fetch 수정
+1. edit_review fetch 수정 
+2. editReviewForm.html js,css파일 경로 수정 
--- src/javascript/edit_review.js
+++ src/javascript/edit_review.js
@@ -1,12 +1,17 @@
+const localEdit = "http://localhost:3500";
+
 const fetchEditData = async () => {
-  const editData = await fetch(location.href + "/json") //
+  const id_edit = location.href.split("update/")[1];
+  const editData = await fetch(`${localEdit}/review/updateData/${id_edit}`) //
     .then((data) => data.json())
     .catch((err) => console.log(err));
 
   showEditData(editData[0]);
 };
 
-const handleReviewEdit = async () => {
+const handleClickEditBtn = async () => {
+  const id_edit = location.href.split("update/")[1];
+
   const titleValue = document.querySelector(".edit_title").value;
   const subTitleValue = document.querySelector(".edit_sub_title").value;
   const categoryValue = document.querySelector(".edit_category").value;
@@ -28,7 +33,7 @@ const handleReviewEdit = async () => {
     },
   };
   location.href = `/review/update/${editDataset}`;
-  await fetch(location.href + "/json", opt);
+  await fetch(`${localEdit}/review/updateData/${id_edit}/click`, opt);
 };
 
 const showEditData = (review) => {
@@ -45,7 +50,7 @@ const showEditData = (review) => {
   editContent.innerText = review.content;
   editCategory.value = review.category;
 
-  editBtn.addEventListener("click", handleReviewEdit);
+  editBtn.addEventListener("click", handleClickEditBtn);
 };
 
 const editInit = () => {
