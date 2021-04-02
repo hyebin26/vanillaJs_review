4b0c3acfa1ccc31a89e05bef0f4f792cb04c9ca0
(hyebin) <(sksp4334@naver.com)>
2021-04-02 14:48:12 +0900

    all html files move to views directory

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ### 이번에 한 것
 
-1. github deploy error #9
-2. html파일 위치변경
-3. package.json 에 homepage 추가
+1. 페이지 배포 heroku로 변경 => 이유 : node js사용 불가 dynamic route불가
+2. views로 html파일 경로 수정
--- server.js
+++ server.js
@@ -4,37 +4,37 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/update/:id", (req, res) => {
-  res.sendFile(__dirname + "/update.html");
+  res.sendFile(__dirname + "/views/update.html");
 });
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/login.html");
+  res.sendFile(__dirname + "/views/login.html");
 });
 app.get("/new", function (req, res) {
-  res.sendFile(__dirname + "/addReviewForm.html");
+  res.sendFile(__dirname + "/views/addReviewForm.html");
 });
 app.get("/signUp", (req, res) => {
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/views/signup.html");
 });
 app.get("/profile", (req, res) => {
-  res.sendFile(__dirname + "/profile.html");
+  res.sendFile(__dirname + "/views/profile.html");
 });
 app.get("/search", (req, res) => {
-  res.sendFile(__dirname + "/search.html");
+  res.sendFile(__dirname + "/views/search.html");
 });
 app.get("/update/:id/edit", (req, res) => {
-  res.sendFile(__dirname + "/editReviewForm.html");
+  res.sendFile(__dirname + "/views/editReviewForm.html");
 });
 app.get("/movie", (req, res) => {
-  res.sendFile(__dirname + "/movie.html");
+  res.sendFile(__dirname + "/views/movie.html");
 });
 app.get("/book", (req, res) => {
-  res.sendFile(__dirname + "/book.html");
+  res.sendFile(__dirname + "/views/book.html");
 });
 app.get("/album", (req, res) => {
-  res.sendFile(__dirname + "/album.html");
+  res.sendFile(__dirname + "/views/album.html");
 });
 
-app.listen(8000, (err) => console.log("server 8000~"));
+app.listen(process.env.PORT || 8000, () => console.log("heroku server~"));
